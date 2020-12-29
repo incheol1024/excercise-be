9b362e3eca464e1a84b9648467c5835b6256b410
ae200202 <yaho1024@etoos.com>
2020-12-29 14:54:51 +0900

    first commit

---
+++ src/main/java/me/incheol/exercisebe/ExerciseBeApplication.java
@@ -0,0 +1,13 @@
+package me.incheol.exercisebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/incheol/exercisebe/ExerciseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.incheol.exercisebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
